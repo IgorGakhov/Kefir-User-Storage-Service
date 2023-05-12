5c5426ed6215415923ac4f0f63b33bac06a402e7
igor_gakhov <gakhov_igor@rambler.ru>
2023-05-12 13:20:06 +0300

    Fix imports

--- src/core/handlers/__init__.py
+++
@@ -1,4 +0,0 @@
-from handlers.exceptions import (
-    internal_exception_handler,
-    client_http_exception_handler
-)
--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
-from src.core.handlers import \
+from src.core.handlers.exceptions import \
     internal_exception_handler, client_http_exception_handler
 from src.auth.router import router as router_auth
 from src.users.router import router_users, router_admin
